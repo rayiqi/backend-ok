52c9bb04d08f316a9cc22802bd49c7a0f8e59751
raiqi1 <116938210+raiqi1@users.noreply.github.com>
2024-09-27 05:52:34 +0700

    Update projectRoutes.ts

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -9,33 +9,6 @@ import {
 import { authenticateUser } from "../middleware/authentication";
 
 const router = Router();
-
-/**
- * @swagger
- * /projects:
- *   get:
- *     summary: Mendapatkan semua proyek
- *     tags: [Projects]
- *     responses:
- *       200:
- *         description: Berhasil mendapatkan daftar proyek
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: string
- *                   name:
- *                     type: string
- *                   description:
- *                     type: string
- *                   author:
- *                     type: string
- */
-
 router.get("/", getProjects);
 router.get("/my-projects", authenticateUser, getProjectsByAuthorUserId);
 router.post("/", authenticateUser, createProject);
