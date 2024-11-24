40c7ce1c17a854939015ddee4133c59bb0ea7ff2
rayiqi <ramadifan769@gmail.com>
2024-11-24 13:33:58 +0700

    oo

--- index.ts
+++ index.ts
@@ -23,7 +23,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 /* ROUTES */
 app.get("/", (req, res) => {
