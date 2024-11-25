855aab6ebcb44a99a9fdd64a14c6be43a68e25c4
raiqi1 <ramadifan769@gmail.com>
2024-11-26 05:02:10 +0700

    add Pathc

--- index.ts
+++ index.ts
@@ -34,7 +34,7 @@ app.use(
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://pm-manage.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
 );
