e7957177f0ffaa5e89b1c309ae6f565b2e4d3525
raiqi1 <ramadifan769@gmail.com>
2024-11-25 20:41:07 +0700

    add cors for FE

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://pm-manage.vercel.app"],
   })
 );
 
